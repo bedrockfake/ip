72bc15361fb490d7c5a3368959b2fe635d1f0262
bedrockfake <kodisiu@gmail.com>
2026-08-17 10:38:16 +0800

    chore(app): rename chatbot from Duke to Luke

---
+++ src/main/java/Luke.java
@@ -0,0 +1,10 @@
+public class Luke {
+    public static void main(String[] args) {
+        String banner = " _         _        \n"
+                + "| |  _   _| | _____ \n"
+                + "| | | | | | |/ / _ \\\n"
+                + "| |_| |_| |   <  __/\n"
+                + "|_____\\__,_|_|\\_\\___|\n";
+        System.out.println(banner);
+    }
+}
